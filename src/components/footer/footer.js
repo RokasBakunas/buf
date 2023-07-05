d019c1aa3b3d03c2779a73c8b4d13ce165f3acbd
rokosius <bakunas.r@gmail.com>
2023-07-05 13:19:18 +0300

    add borders to header and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <>
-    <div className={`fixed text-center p-2 bottom-0 w-full ${styles.footer}`}>
+    <div className={`border-t-2 border-blue-500 fixed text-center p-2 bottom-0 w-full ${styles.footer}`}>
     „BestQuestion“  © visos teisės saugomos
 </div>
     </>
